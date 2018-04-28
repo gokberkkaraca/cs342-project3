bdc7c09b6dd27aea8b64a15aa043aa3373e741bd
Gokberk Karaca <gskaraca1905@gmail.com>
2018-04-29 01:59:26 +0300

    Implement part c

--- mymodule.c
+++ mymodule.c
@@ -9,6 +9,7 @@ static int processid;
 static int is_stack(struct vm_area_struct *vma);
 static int is_heap(struct vm_area_struct *vma);
 void print_memory_area(struct mm_struct *mm, struct vm_area_struct *vma);
+void print_page_tables(struct mm_struct *mm);
 
 int init_module(void)
 {
@@ -27,6 +28,7 @@ int init_module(void)
   mm = task->mm;
   vma = mm->mmap;
   print_memory_area(mm, vma);
+  print_page_tables(mm);
 
   return 0;
 }
@@ -38,27 +40,48 @@ void cleanup_module(void)
 
 void print_memory_area(struct mm_struct *mm, struct vm_area_struct *vma) {
 
-  printk("Code start: %lx\t end: %lx\t size: %lx\n", mm->start_code,
+  printk("Code start: %lx\t end: %lx\t size: %lu\n", mm->start_code,
     mm->end_code, mm->end_code - mm->start_code);
-  printk("Data start: %lx\t end: %lx\t size: %lx\n",
+  printk("Data start: %lx\t end: %lx\t size: %lu\n",
     mm->start_data, mm->end_data, mm->end_data - mm->start_data);
 
   for (; vma != NULL; vma = vma->vm_next) {
     if (is_stack(vma)) {
-      printk("Stack: start: %lx\t end: %lx\t size: %lx\n",
+      printk("Stack: start: %lx\t end: %lx\t size: %lu\n",
        vma->vm_start, vma->vm_end, vma->vm_end - vma->vm_start);
     }else if(is_heap(vma)){
-      printk("Heap: start: %lx\t end: %lx\t size: %lx\n",
+      printk("Heap: start: %lx\t end: %lx\t size: %lu\n",
        vma->vm_start, vma->vm_end, vma->vm_end - vma->vm_start);
     }
   }
 
-  printk("Main Arguments start: %lx\t end: %lx\t size: %lx\n",
+  printk("Main Arguments start: %lx\t end: %lx\t size: %lu\n",
     mm->arg_start, mm->arg_end, mm->arg_end - mm->arg_start);
-  printk("Environment Variables start: %lx\t end: %lx\t size: %lx\n",
+  printk("Environment Variables start: %lx\t end: %lx\t size: %lu\n",
     mm->env_start, mm->env_end, mm->env_end - mm->env_start);
   printk("number of frames used by the process (rss): %lx\t", get_mm_rss(mm));
   printk("Total virtual memory used by the process (total_vm): %lx\t", mm->total_vm);
+}
+
+void print_page_tables(struct mm_struct *mm) {
+  int i;
+  pgd_t *pgd = mm->pgd;
+  for (i = 0; i < 512; i++) {
+    unsigned long address;
+    address = pgd[i].pgd;
+    if ( address != 0) {
+      printk("\nEntry %d", i);
+      printk("PGD: %#018lx\t", address);
+      printk("P: %lu\t", (address << 63) >> 63);
+      printk("R/W: %lu\t", (address << 62) >> 63);
+      printk("U/S: %lu\t", (address << 61) >> 63);
+      printk("PWT: %lu\t", (address << 60) >> 63);
+      printk("PCD: %lu\t", (address << 59) >> 63);
+      printk("A: %lu\t", (address << 58) >> 63);
+      printk("PS: %lu\t", (address << 56) >> 63);
+      printk("Physical adress: %#08lx\t", (address << 28) >> 40);
+    }
+  }
 }
 
 /* This method is taken from linux kernel source code */
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ sudo insmod $1.ko processid=$2
 sudo rmmod $1
 
 #output the log file
-tail -15 /var/log/kern.log
+tail -100 /var/log/kern.log
 
 #clean files
 rm .$1*
