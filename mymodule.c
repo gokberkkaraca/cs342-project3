5e9d6265e6ed0a77eff73ce2bfe15405ab707c30
Gokberk Karaca <gskaraca1905@gmail.com>
2018-04-30 16:31:58 +0300

    Write address in hex

--- mymodule.c
+++ mymodule.c
@@ -70,7 +70,7 @@ void print_page_tables(struct mm_struct *mm) {
   for (i = 0; i < 512; i++) {
     unsigned long address;
     address = pgd[i].pgd;
-    if ((address & 1) == 1) {
+    if ((address & 0x00000001) == 1) {
       printk("Entry %d", i);
       printk("PGD: %#018lx\t", address);
       printk("P: %lu\t", (address << 63) >> 63);
